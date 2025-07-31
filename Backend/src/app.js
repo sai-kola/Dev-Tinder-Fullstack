c3651bfcec2f7319abdbe716901e66bb896a096a
saikola24 <saikola0036@gmail.com>
2025-07-31 22:31:55 +0530

    cors issue

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -9,7 +9,7 @@ require("dotenv").config();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173","https://dev-tinder-fullstack.vercel.app"],
     credentials: true,
   })
 );
